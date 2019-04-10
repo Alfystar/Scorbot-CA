f5b7f4c9d1deacbad955234f90ad6afa3f5e2533
yanzdp <48930400+yanzdp@users.noreply.github.com>
2019-04-10 18:03:37 +0200

    Update main.cpp

--- rasp-Cpp/Scorbot_CPP/main.cpp
+++ rasp-Cpp/Scorbot_CPP/main.cpp
@@ -45,8 +45,8 @@ int main()
 void printSPIPACK(SPIPACK *s){
     switch(s ->type){
     case setPWM:
-        printf("Pack type: setPWM");
-        printf("Vel Send:\n1)%d\t2)%d\t3)%d\t4)%d\t5)%d\t6)%d\n",s->out.pack.speed.vel[cMot1],
+        /*printf("Pack type: setPWM");
+       printf("Vel Send:\n1)%d\t2)%d\t3)%d\t4)%d\t5)%d\t6)%d\n",s->out.pack.speed.vel[cMot1],
                s->out.pack.speed.vel[cMot2],
                s->out.pack.speed.vel[cMot3],
                s->out.pack.speed.vel[cMot4],
@@ -57,43 +57,113 @@ void printSPIPACK(SPIPACK *s){
                s->in.pack.en.passi[cMot3],
                s->in.pack.en.passi[cMot4],
                s->in.pack.en.passi[cMot5],
-               s->in.pack.en.passi[cMot6]);
+               s->in.pack.en.passi[cMot6]);*/
+            
+       printf("Sending actual speed:\n");
+       printf("Vel:\t"); 
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.speed.vel[cMot1 + i]);
+           j++;
+       }         
+            
+            
+       printf("Reciving actual encoder:\n");
+       printf("En:\t"); 
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.en.passi[cMot1 + i]);
+           j++;
+       }      
+            
         printf("-------------------------------------------------------\n");
         break;
 
     case getCurrent:
         printf("Sending: no parameters");
-        printf("Current read:\n1)%hd\t2)%hd\t3)%hd\t4)%hd\t5)%hd\t6)%hd\n",s->in.pack.cur.current[cMot1],
+        /* printf("Current read:\n1)%hd\t2)%hd\t3)%hd\t4)%hd\t5)%hd\t6)%hd\n",s->in.pack.cur.current[cMot1],
                s->in.pack.cur.current[cMot2],
                s->in.pack.cur.current[cMot3],
                s->in.pack.cur.current[cMot4],
                s->in.pack.cur.current[cMot5],
-               s->in.pack.cur.current[cMot6]);
-               printf("-------------------------------------------------------\n");
+               s->in.pack.cur.current[cMot6]);*/
+       printf("Reciving actual currents:\n");
+       printf("Curr:\t"); 
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.cur.current[cMot1 + i]);
+           j++;
+       }        
+            
+       printf("-------------------------------------------------------\n");
                 break;
 
     case getSetting:
-        /*printf("Properties Send:\n1)%d\t2)%d\t3)%d\t4)%d\t5)%d\t6)%d\n",s->in.pack.prop.sets[cMot1],
-               s->in.pack.prop.sets[cMot2],
-               s->in.pack.prop.sets[cMot3],
-               s->in.pack.prop.sets[cMot4],
-               s->in.pack.prop.sets[cMot5],
-               s->in.pack.prop.sets[cMot6]);
-        */
-        printf("Reciving: no parameters");
+            
+       printf("Sending: no parameters");
+            
+       printf("Reciving actual settings:\n");
+       printf("maxEn:\t"); 
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.prop.maxEn[cMot1 + i]);
+           j++;
+       }      
+            
+            
+        printf("minEn:\t");
+        int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.prop.minEn[cMot1 + i]);
+           j++;
+       }   
+            
+       printf("maxCurrMed:\t");
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.prop.maxCurrMed[cMot1 + i]);
+           j++;
+       }   
+      
                printf("-------------------------------------------------------\n");
         break;
+            
+            
+            
+            
     case setSetting:
-        printf("Sending: no parameters");
-       /* printf("Properties read:\n1)%hd\t2)%hd\t3)%hd\t4)%hd\t5)%hd\t6)%hd\n",s->in.pack.prop.sets[cMot1],
-               s->in.pack.prop.sets[cMot2],
-               s->in.pack.prop.sets[cMot3],
-               s->in.pack.prop.sets[cMot4],
-               s->in.pack.prop.sets[cMot5],
-               s->in.pack.prop.sets[cMot6]);
-               */
-               printf("-------------------------------------------------------\n");
+      
+       printf("Sending actual settings:\n");
+            
+       printf("maxEn:\t"); 
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.prop.maxEn[cMot1 + i]);
+           j++;
+       }      
+            
+            
+        printf("minEn:\t");
+        int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.prop.minEn[cMot1 + i]);
+           j++;
+       }   
+            
+       printf("maxCurrMed:\t");
+       int j=1;     
+       for (byte i = 0; i < nMot; i++){
+           printf("%d)\t%hd\t",j,s->in.pack.prop.maxCurrMed[cMot1 + i]);
+           j++;
+       }   
+             
+        printf("Reciving: no parameters");
+        printf("-------------------------------------------------------\n");
         break;
+            
+            
+            
+            
     case goHome:
         printf("Sending: no parameters");
         printf("Reciving: no parameters");
