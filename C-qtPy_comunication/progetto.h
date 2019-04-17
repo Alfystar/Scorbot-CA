aa690b64b4ba85ec47b07c172139b30d1e3b11f5
fra <ffrancesco96@hotmail.it>
2019-04-17 17:14:41 +0200

    Aggiornamento riferito alle richieste C e Py
    Ottimizzazioni e aggiornamento delle funzioni invia e ricevi dei rispettivi ambienti con eliminazione di funzioni superflue quali componi_mes()

---
+++ C-qtPy_comunication/progetto.h
@@ -0,0 +1,55 @@
+#ifndef PROGETTO_H
+#define PROGETTO_H
+
+struct impostazioni{
+  int n;  //numero impostazioni
+  char imp1[128];  
+  char imp2[128];
+}impostazioni;
+ 
+struct messaggio{
+  int enc[6];
+  int cor[6];
+  struct impostazioni *imp;
+}messaggio;
+
+struct uten{  //nodo della lista collegata
+  struct messaggio *data; 
+  struct uten *p;
+};
+
+struct uten* inserisci_u(struct uten *testa,struct uten *mes);
+
+struct uten* libera_m(struct uten *nodo);
+
+void libera_u(struct uten *testa);
+
+struct uten* libera_m(struct uten *nodo);
+
+int invia(struct messaggio *m,char *tipo);
+
+struct messaggio* parsing(char* text, char* p);
+
+void *readerPipe ();
+
+int main_com(char *file_p[]);
+
+
+
+//controllore
+
+void invia_cont(struct messaggio* m);
+
+struct messaggio* ricevi_cont();
+
+void invia_rs(struct messaggio *m);
+
+struct messaggio* ricevi_rs();
+
+void libera_semafori();
+
+int inizializza_semafori();
+
+
+
+#endif
