b813710472e7cf229f47240b71dd9ae86a28b516
albertomenichelli <menichelli.alverto@libero.it>
2019-04-18 17:32:23 +0200

    Aggiornamento importante 18-04-2019

--- pyUi/ScorbotUI/myUiClass.py
+++ pyUi/ScorbotUI/myUiClass.py
@@ -3,11 +3,11 @@ from typing import List
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QLabel
 
-from pyqtLib.UiClass import UiClass
+from pyqtLib.UiClass import Ui_UiClass
 
 from pyqtLib.pyC_com.p import *
 
-class MyUiQt (UiClass):
+class MyUiQt (Ui_UiClass):
 
     def __init__(self, Windows):
         super().setupUi(Windows)   # genera gli oggetti dell'interfaccia grafica
@@ -17,6 +17,8 @@ class MyUiQt (UiClass):
         # la seguente lista contiene tutti i pulsanti Encoder della prima pagina
         self.encoder1 = [self.riferimento1_direct.value(), self.riferimento1_2_direct.value(), self.riferimento1_3_direct.value(),self.riferimento1_4_direct.value(), self.riferimento1_5_direct.value(), self.riferimento1_6_direct.value()]
         self.setup = [int(self.passi1_direct.text()),int(self.passi1_2_direct.text()), int(self.passi1_3_direct.text()), int(self.passi1_4_direct.text()), int(self.passi1_5_direct.text()), int(self.passi1_6_direct.text())]
+        self.encr = [int(self.encrease_value1.text()),int(self.encrease_value2.text()), int(self.encrease_value3.text()), int(self.encrease_value4.text()), int(self.encrease_value5.text()), int(self.encrease_value6.text()) ]
+
 
         """LISTA Setup"""
         self.riferimento1_direct.setMaximum(20000) # questo, e le seccessive 5 righe, fissano i valori massimi
@@ -27,6 +29,7 @@ class MyUiQt (UiClass):
         self.riferimento1_6_direct.setMaximum(20000)
 
 
+
         self.riferimento1_direct.setMinimum(-20000) # questo, e le seccessive 5 righe, fissano i valori minimi
         self.riferimento1_2_direct.setMinimum(-20000) # che posso inserire all'interno degli ecoder
         self.riferimento1_3_direct.setMinimum(-20000)
@@ -34,9 +37,12 @@ class MyUiQt (UiClass):
         self.riferimento1_5_direct.setMinimum(-20000)
         self.riferimento1_6_direct.setMinimum(-20000)
 
+
         """LISTA Eventi"""
 
         self.Pulsante_invio_pagina_Basics.clicked.connect(self.encoderValue) #premendo invio, metto nella lista i passi encoder da aggiungere  quelli qttuali
+
+
 
     def encoderValue(self):
 
@@ -63,6 +69,8 @@ class MyUiQt (UiClass):
         self.passi1_5_direct.setText(str(self.encoder1[4]))
         self.passi1_6_direct.setText(str(self.encoder1[5]))
 
+
+
         encrease = []  #la variabile encrease mi permette di stampare l'incremento effettivo di ogni encoder
 
         encrease.append(self.encoder1[0] - self.setup[0])
@@ -72,6 +80,16 @@ class MyUiQt (UiClass):
         encrease.append(self.encoder1[4] - self.setup[4])
         encrease.append(self.encoder1[5] - self.setup[5])
 
+
+        self.encrease_value1.setText(str(encrease[0]))
+        self.encrease_value2.setText(str(encrease[1]))
+        self.encrease_value3.setText(str(encrease[2]))
+        self.encrease_value4.setText(str(encrease[3]))
+        self.encrease_value5.setText(str(encrease[4]))
+        self.encrease_value6.setText(str(encrease[5]))
+
+
+
         self.setup[0] = int(self.passi1_direct.text())
         self.setup[1] = int(self.passi1_2_direct.text())
         self.setup[2] = int(self.passi1_3_direct.text())
@@ -80,14 +98,6 @@ class MyUiQt (UiClass):
         self.setup[5] = int(self.passi1_6_direct.text())
 
 
-
-        for i in range(1, 7):           #nel seguente for, c'è un if per determinare e è stato fatto un decremento o un incremento del valore dell'encoder
-            if encrease[i-1] >= 0 :
-                print("set Encoder ", i, "on:", self.encoder1[i-1],"current encrease:", encrease[i-1]) # stampa a schermo la lista l
-
-            else:
-
-                print("set Encoder ", i, "on:", self.encoder1[i - 1], "current decrease:", encrease[i - 1])
 
 
 
