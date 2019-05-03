919c6c13331e2510dc5eb585cd4ef0bf721625b7
alfystar <alfystar1701@gmail.com>
2019-05-03 18:03:51 +0200

    pid all mot

--- pidWork/pidClassDemo/PidClass/PIDScorbot.cpp
+++ pidWork/pidClassDemo/PidClass/PIDScorbot.cpp
@@ -47,17 +47,18 @@ int PIDScorbot::motVal(int ref,int en)
     this->oldTemp=this->temp;
     gettimeofday(&this->temp,NULL);
 
-    int er = (ref - en)*(1-(2*this->posDir)); //per allineare verso dei pwm a incremento degli encoder
-
     if(this->temp.tv_usec-this->oldTemp.tv_usec<0); //se avviene l'overflow ipotizzo uguale a prima
     else ts=this->temp.tv_usec-this->oldTemp.tv_usec;
+
+    int er = (ref - en)*(1-(2*this->posDir)); //per allineare verso dei pwm a incremento degli encoder
+    //if(abs(er)<150) return hs;
 
     float vOut=this->PIDComp(er,ts);
     printf("vOut=%f\n",vOut);
 
     if(fabsf(vOut)<this->CONTROL_DEADZONE)
     {
-        return fr; //soft stop
+        return ss; //free running
     }else{
         if(vOut>0) return int(fmap(vOut,0.0,1.0,this->MOTOR_DEADZONE,255)+0.5);
         else
@@ -99,7 +100,6 @@ float PIDScorbot::UpdateSat(float x, float dx, float a, float k, float s, float 
 
 float PIDScorbot::PIDComp(int error, __suseconds_t Ts)
 {
-    printf("error=%d, dt=%ld\n",error,Ts);
     /*Il pid calcola un valore in uscita tra -1.0 e 1.0 che è -100% to 100% della pot di uscita del motore*/
     //TS tempo campione in micro secondi
 
@@ -117,7 +117,7 @@ float PIDScorbot::PIDComp(int error, __suseconds_t Ts)
 
     this->x_i += this->UpdateSat(this->x_i, this->Ki * Ts * this->mystack[6].er, this->Kp * error + this->y_d, 1, this->CONTROL_DEADZONE, this->CONTROL_SATURATION);
 
-    printf("x_i = %f, y_d = %f, error = %d, u = %f\n",x_i,y_d,error,(Kp*error + x_i + y_d));
+    printf("x_i = %f, y_d = %f, error = %d, u = %f, dt=%ld\n",x_i,y_d,error,(Kp*error + x_i + y_d),Ts);
 
     return ( std::min(this->CONTROL_SATURATION, std::max(-this->CONTROL_SATURATION, this->Kp * error + this->x_i + this->y_d )));
 }
