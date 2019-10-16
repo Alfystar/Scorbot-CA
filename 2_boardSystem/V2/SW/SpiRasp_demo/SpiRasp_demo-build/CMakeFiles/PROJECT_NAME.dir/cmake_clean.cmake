424af05797136b557cd323e5b2797d62727862f9
alfystar <alfystar1701@gmail.com>
2019-10-16 10:23:36 +0200

    save old documetation

---
+++ 2_boardSystem/V2/SW/SpiRasp_demo/SpiRasp_demo-build/CMakeFiles/PROJECT_NAME.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROJECT_NAME.dir/main.cpp.o"
+  "CMakeFiles/PROJECT_NAME.dir/home/scorebot/Scorbot-CA/2_boardSystem/V2/SW/SpiPack/SpiPack.cpp.o"
+  "CMakeFiles/PROJECT_NAME.dir/SPILib/SpiRaspInterface.cpp.o"
+  "PROJECT_NAME.pdb"
+  "PROJECT_NAME"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PROJECT_NAME.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
