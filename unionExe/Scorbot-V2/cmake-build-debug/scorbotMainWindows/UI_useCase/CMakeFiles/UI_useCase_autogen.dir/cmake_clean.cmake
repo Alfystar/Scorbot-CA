c287f929dc10cf8995253574a46abeea92f32a8c
alfystar <alfystar1701@gmail.com>
2020-04-07 00:54:18 +0200

    test ssh

--- unionExe/Scorbot-V2/cmake-build-debug/scorbotMainWindows/UI_useCase/CMakeFiles/UI_useCase_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/UI_useCase_autogen"
-  "UI_useCase_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/UI_useCase_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
